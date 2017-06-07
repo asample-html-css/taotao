44df345c3bb1b59839999d04dd6efd36c3d8abf6
dd876799869 <876799869@qq.com>
2017-06-07 23:03:42 +0800

    内容分类crud

--- taotao-manage-service/src/main/java/com/taotao/manage/service/ContentCategoryService.java
+++ taotao-manage-service/src/main/java/com/taotao/manage/service/ContentCategoryService.java
@@ -3,9 +3,55 @@ package com.taotao.manage.service;
 import com.taotao.manage.pojo.ContentCategory;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Administrator on 2017/6/7 0007.
  */
 @Service
 public class ContentCategoryService extends BaseService<ContentCategory> {
+
+    /**
+     * 删除节点
+     * @param contentCategory
+     */
+    public void deleteAll(ContentCategory contentCategory) {
+        //创建容器，所有需要删除的节点的id封装进来
+        List<Object> ids = new ArrayList<Object>();
+        ids.add(contentCategory.getId());
+        //递归查询该节点所辖所有节点
+        this.findAllSubNodeIds(ids,contentCategory.getId());
+        super.deleteByIds(ids,ContentCategory.class,"id");
+
+        //判断有没有兄弟节点  若没有则将父节点改为子节点
+        ContentCategory record = new ContentCategory();
+        record.setParentId(contentCategory.getParentId());
+        List<ContentCategory> list = super.queryListByWhere(record);
+        if (null == list ||list.isEmpty() ){
+            ContentCategory parentRecord = new ContentCategory();
+            parentRecord.setId(contentCategory.getParentId());
+            parentRecord.setIsParent(false);
+            super.updateSelective(parentRecord);
+        }
+    }
+
+    /**
+     *  递归查询该节点所辖所有节点
+     * @param ids
+     * @param pid
+     */
+    private void findAllSubNodeIds(List<Object> ids, Long pid) {
+        ContentCategory record = new ContentCategory();
+        record.setParentId(pid);
+        List<ContentCategory> list = super.queryListByWhere(record);
+        for (ContentCategory contentCategory:list) {
+            //首先将自己放进去
+            ids.add(contentCategory.getId());
+            //如果自己是父节点的话  开始递归 将id当成parentId
+            if (contentCategory.getIsParent()){
+                findAllSubNodeIds(ids, contentCategory.getId());
+            }
+        }
+    }
 }
--- taotao-manage-web/src/main/java/com/taotao/manage/controller/ContentCategoryController.java
+++ taotao-manage-web/src/main/java/com/taotao/manage/controller/ContentCategoryController.java
@@ -1,7 +1,16 @@
 package com.taotao.manage.controller;
 
+import com.taotao.manage.pojo.ContentCategory;
+import com.taotao.manage.service.ContentCategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 
 /**
@@ -10,6 +19,96 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("content/category")
 public class ContentCategoryController {
+    @Autowired
+    private ContentCategoryService contentCategoryService;
+
+    /**
+     * 查询内容分类列表
+     *
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<ContentCategory>> queryContentCategoryListByParentId(
+            @RequestParam(value = "id", defaultValue = "0") Long pid) {
+        try {
+            ContentCategory record = new ContentCategory();
+            record.setParentId(pid);
+            List<ContentCategory> list = this.contentCategoryService.queryListByWhere(record);
+            if (null == list || list.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            }
+            return ResponseEntity.ok(list);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+    }
+
+
+    /**
+     * 新增子节点
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<ContentCategory> saveContentCategory(ContentCategory contentCategory) {
+        try {
+            //保存子节点
+            contentCategory.setId(null);
+            contentCategory.setIsParent(false);
+            contentCategory.setStatus(1);
+            contentCategory.setSortOrder(1);
+            this.contentCategoryService.saveSelective(contentCategory);
+
+            //判断现在的父节点的isParent是否是true  如果不是则改为true
+            ContentCategory parent = contentCategoryService.queryById(contentCategory.getParentId());
+            if (!parent.getIsParent()){
+                parent.setIsParent(true);
+                this.contentCategoryService.updateSelective(parent);
+            }
+            return ResponseEntity.status(HttpStatus.CREATED).body(contentCategory);//400
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+    }
+
+    /**
+     * 重命名节点
+     *
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.PUT)
+    public ResponseEntity<Void> rename(@RequestParam("id") Long id,@RequestParam("name") String name) {
+        try {
+            ContentCategory record = new ContentCategory();
+            record.setId(id);
+            record.setName(name);
+            this.contentCategoryService.updateSelective(record);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+    }
+
+
+
+    /**
+     * 删除节点 1，删除自己  2，删除所辖子节点   3，更改父节点的状态
+     *
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.DELETE)
+    public ResponseEntity<Void> deleteNode(ContentCategory contentCategory) {
+        try {
+            this.contentCategoryService.deleteAll(contentCategory);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+    }
+
 
 
 
