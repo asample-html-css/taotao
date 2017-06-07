b47c4fe7e6bba25f9c0b2fd88bba0d40c9132ee1
dd876799869 <876799869@qq.com>
2017-06-08 07:22:31 +0800

    内容分类crud

--- taotao-manage-web/src/main/java/com/taotao/manage/controller/ContentController.java
+++ taotao-manage-web/src/main/java/com/taotao/manage/controller/ContentController.java
@@ -1,7 +1,18 @@
 package com.taotao.manage.controller;
 
+import com.taotao.manage.pojo.Content;
+import com.taotao.manage.pojo.ContentCategory;
+import com.taotao.manage.service.ContentCategoryService;
+import com.taotao.manage.service.ContentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 
 /**
@@ -11,6 +22,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("content")
 public class ContentController {
 
+    @Autowired
+    private ContentService contentService;
+
+    /**
+     * 新增内容
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Content> saveContentCategory(Content content) {
+        try {
+            content.setId(null);
+            this.contentService.saveSelective(content);
+            return ResponseEntity.status(HttpStatus.CREATED).body(content);//400
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+    }
 
 
 }
